b50f4df5bd5e4911bd8d16b6d6d769d243550ce6
Yebin Kim <rladpqls07@gmail.com>
2022-08-13 14:20:41 +0900

    Fix: clock code

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,12 @@
 const clock = document.querySelector("#clock");
 const dateSpan = document.querySelector('#date');
-const date = new Date();
 
 const pad = (num) => {
     return String(num).padStart(2, '0'); // 두 자릿수 -> 0으로 채우기
 }
 
 const getClock = () => {
+    const date = new Date();
     clock.innerText = `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
 };
 
@@ -16,6 +16,7 @@ setInterval(getClock, 1000);
 
 // 날짜 추가
 const getDate = () => {
+    const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth();
     const nowdate = date.getDate();
